a9b1e642a8a5296b9e93a140f7e30bb448436647
dragon <270616503@qq.com>
2022-09-14 15:18:42 +0800

    修改签名文件

--- app/src/main/java/com/dragon/toolbox/RouterActivity.kt
+++ app/src/main/java/com/dragon/toolbox/RouterActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dragon.toolbox.scroll.ScrollActivity
-import com.jakj.downloader.DownloadManager
 
 class RouterActivity : AppCompatActivity() {
 
